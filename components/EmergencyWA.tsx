56482a72b058d3ac684a33e5c2497e9882a467a0
alieffaisaladriansyah <alieffaisal6@gmail.com>
2026-04-20 03:45:41 +0700

    update

--- components/EmergencyWA.tsx
+++ components/EmergencyWA.tsx
@@ -5,7 +5,7 @@ import { usePathname } from "next/navigation";
 const EmergencyWA = () => {
   const pathname = usePathname();
 
-  // Hanya render jika berada di halaman beranda '/'
+
   if (pathname !== "/") return null;
 
   return (
@@ -14,10 +14,10 @@ const EmergencyWA = () => {
         href="https://wa.me/6282246232527"
         target="_blank"
         rel="noopener noreferrer"
-        className="pointer-events-auto absolute bottom-[30px] right-[-65px] block w-[250px] bg-[#007e00] hover:bg-[#016101] text-white text-center py-2.5 text-[14px] font-extrabold uppercase tracking-wider shadow-xl transform rotate-[-45deg] transition-colors duration-200 font-sans"
+        className="pointer-events-auto absolute bottom-[30px] right-[-65px] block w-[250px] bg-[#005cb3] hover:bg-[#005cb3]/90 text-white text-center py-2.5 text-[12px] font-bold uppercase tracking-wider shadow-xl transform rotate-[-45deg] transition-colors duration-200 font-sans"
       >
-        Whatsapp
-        {/* Segitiga di sisi kanan bawah */}
+        Hubungi kami
+        {/* button */}
         <span className="absolute bottom-0 right-0 w-0 h-0 border-solid border-t-0 border-l-0 border-b-[20px] border-r-[20px] border-b-[#075e54] border-r-transparent"></span>
       </a>
     </div>
