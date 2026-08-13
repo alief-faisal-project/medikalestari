63cbc3d177d2759781d2dc5d260d3e283c1c84fe
alieffaisaladriansyah <alieffaisal6@gmail.com>
2026-08-13 12:26:43 +0700

    update

--- app/tentang-kami/page.tsx
+++ app/tentang-kami/page.tsx
@@ -3,12 +3,15 @@ import AboutUs from "@/components/AboutUs";
 export const metadata = {
   title: "Tentang Kami",
   description: "Sejarah, Visi, Misi, dan Profil Lengkap RS Medika Lestari",
+  robots: {
+    index: false,
+    follow: false,
+  },
 };
 
 export default function TentangKamiPage() {
   return (
     <main className="min-h-screen">
-
       {/* Memanggil Komponen AboutUs */}
       <AboutUs />
     </main>
